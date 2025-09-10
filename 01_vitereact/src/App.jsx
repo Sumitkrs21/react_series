d191c19b8859ea68106f92b9c028c1d97a8ace54
Sumitkrs21 <sumitzr1977@gmail.com>
2025-09-10 22:10:30 +0530

    creating of project via create react and vite

---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from "./test.jsx";
+function App() {
+  return (
+    <>
+      <Test/>
+      <h1>hello app</h1>
+      <p>This is my first react app</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01_vitereact/src/test.jsx
@@ -0,0 +1,7 @@
+function Test(){
+    return(
+        <h1>hello test</h1>
+    )
+}
+
+export default Test
